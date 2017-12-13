73c78afbed1c0036ae48cbca5a23a6ff09382982
wightman <davidwightmanmusic@gmail.com>
2017-12-13 12:06:58 -0500

    refactored solution

--- problem1/problem1.js
+++ problem1/problem1.js
@@ -1,6 +1,7 @@
-(function sumOfMultiples() {
+var maxValue = 1000;
+
+function sumOfMultiples(max) {
 	var result = 0;
-	var max = 1000;
 
 	for (var i = 1; i < max; i++) {
 		if (i % 3 == 0 || i % 5 == 0) {
@@ -8,5 +9,7 @@
 		}
 	}
 
-	console.log(result);
-})();
+	return result;
+}
+
+console.log(sumOfMultiples(maxValue));
